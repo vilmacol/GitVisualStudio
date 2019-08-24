ab1cb117826a7c32567956a10b20bd5bd2e495b3
vilmacol <vilmacolman17@gmail.com>
2019-08-23 20:12:57 -0400

    Segundo cambio

--- PruebaGit/Program.cs
+++ PruebaGit/Program.cs
@@ -11,6 +11,7 @@ namespace PruebaGit
         static void Main(string[] args)
         {
             Console.WriteLine("Primer cambio");
+            Console.WriteLine("Segundo cambio");
 
         }
     }
